d43dcec212f14e7c6ed5cb63cc30bcaa4be7e2b1
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-05-07 10:47:36 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,12 @@
 // write code for Welcome component here
 // write code for Welcome component here
-const Welcome = ({ name }) => {
-  return;
+function Welcome(name) {
+  return (
   <details>
     <h1>Hey {name}!</h1>
     <h2>Welcome to Newton School.</h2>
   </details>;
-};
+);
+}
 export default Welcome;
 
